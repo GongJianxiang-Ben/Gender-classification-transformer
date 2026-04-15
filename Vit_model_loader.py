de9810da96f5b4736798e5ad678178662b5cc35b
#GONG JIANXIANG# <n2500490j@CCDS-TC1.cm.cluster>
2026-04-15 23:36:26 +0800

    pack all useful functions in utils.py and update relevant files

---
+++ Vit_model_loader.py
@@ -0,0 +1,140 @@
+from transformers import AutoImageProcessor
+from transformers import  AutoModelForImageClassification
+from train_vit import ViT
+from model_deformConv import CustomDeformViT
+from model_dilatedConv import CustomDilatedViT
+from model_addCNN import EnhancedViTForImageClassification
+from model_VPTshallow import ShallowVPTViTForImageClassification
+from model_VPTdeep import DeepVPTViTForImageClassification
+from model_depth import DeepViT
+from adience_data_loader import CustomImageDataset,ImageClassificationCollator
+from Dataset.utk_data_loader import UTKFaceDataset
+import argparse
+from sklearn.model_selection import train_test_split
+
+import json
+feature_extractor = AutoImageProcessor.from_pretrained("rizvandwiki/gender-classification")
+
+from pathlib import  Path
+import math
+from torch.utils.data import ConcatDataset
+from torch.utils.data import DataLoader, Subset
+import torch
+from torchvision import transforms
+from Dataset.utk_data_loader import UTKFaceDataset
+from celebA_data_loader import CelebAGenderDataset
+from utils import tester
+parser = argparse.ArgumentParser()
+parser.add_argument("--img_dir",     type=str, required=True)
+parser.add_argument("--attr_folder",   type=str, required=True)
+parser.add_argument("--dataset",  type=str, required=True)
+#which dataset to use for testing
+parser.add_argument("--start",  type=str, required=True) 
+#the checkpoint to start from
+parser.add_argument("--model_name",  type=str, required=True) 
+#the model type to use
+parser.add_argument("--backbone",  type=str, required=True) 
+#the backbone model location "online","local"
+parser.add_argument("--address",  type=str, required=True) 
+#the address of the backbone
+parser.add_argument("--batch_size",  type=int, default=64)
+parser.add_argument("--seed",        type=int, default=42)
+parser.add_argument("--num_workers", type=int, default=4)
+args = parser.parse_args()
+print(json.dumps(vars(args), indent=4))
+TEST_RATIO = 0.2   # 20% as test set
+SEED       = 42    # fixed seed for reproducibility
+
+#use model.model to extract the ViTForImageClassification model from class
+model_classes = {
+    "ViT":          ViT,
+    "deformConv":   CustomDeformViT,
+    "dilatedConv":  CustomDilatedViT,
+    "addCNN":       EnhancedViTForImageClassification,
+    "VPTshallow":   ShallowVPTViTForImageClassification,
+    "VPTdeep":      DeepVPTViTForImageClassification,
+    "depth":        DeepViT,
+    "online":       None,   
+}
+
+if args.model_name not in model_classes:
+    raise ValueError(f"Unknown model name: {args.model_name}")
+
+cls = model_classes[args.model_name]
+if args.model_name == "online":
+    model = AutoModelForImageClassification.from_pretrained("rizvandwiki/gender-classification")
+else:
+    if(args.backbone!="None"):
+        model = cls.load_from_checkpoint(
+            args.start,
+            epochs=4,
+            num_classes=2,
+            model_name=args.address,
+            config=args.backbone
+        )
+    else:
+        model = cls.load_from_checkpoint(
+            args.start,
+            epochs=4,
+            num_classes=2
+        )
+"""if(args.model_name=="ViT"): 
+    model=ViT.load_from_checkpoint(args.start,epochs=4,num_classes=2,config=config)
+elif(args.model_name=="deformConv"):
+    model=CustomDeformViT.load_from_checkpoint(args.start)
+elif(args.model_name=="dilatedConv"):
+    model=CustomDilatedViT.load_from_checkpoint(args.start)
+elif(args.model_name=="addCNN"):
+    model=EnhancedViTForImageClassification.load_from_checkpoint(args.start,
+    epochs=4,num_classes=2,config=config)
+elif(args.model_name=="VPTshallow"):
+    model=ShallowVPTViTForImageClassification.load_from_checkpoint(args.start,
+    epochs=4,num_classes=2,config=config)
+elif(args.model_name=="VPTdeep"):
+    model=DeepVPTViTForImageClassification.load_from_checkpoint(args.start,
+    epochs=4,num_classes=2,config=config)
+elif(args.model_name=="depth"):
+    model=DeepViT.load_from_checkpoint(args.start,epochs=4,num_classes=2,config=config)
+elif(args.model_name=="online"):
+    model=AutoModelForImageClassification.from_pretrained("rizvandwiki/gender-classification")"""
+train_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        
+    ])
+val_tf = transforms.Compose([
+    transforms.Resize((224, 224)),
+    
+])
+txt_dir="./label txt"
+folder = Path(txt_dir)
+paths = [str(p) for p in folder.iterdir()]
+K = len(paths)
+avg_acc=0
+if(args.dataset=="adience"):
+    test_ds=CustomImageDataset(img_dir=args.img_dir,
+                          txt_file=paths[0],transform=val_tf)
+elif(args.dataset=="UTK"):
+    full_ds = UTKFaceDataset(img_dir=args.img_dir, align_with_adience=True)
+    test_ds=full_ds
+    # fixed 20% split — same split every run due to fixed seed
+    """indices = list(range(len(full_ds)))
+    _, test_indices = train_test_split(
+        indices, test_size=TEST_RATIO, random_state=SEED
+    )
+    test_ds = Subset(full_ds, test_indices)"""
+elif(args.dataset=="celebA"):
+    test_ds=CelebAGenderDataset(args.img_dir, args.attr_file, args.split_file, split=2, transform=val_tf)
+collator = ImageClassificationCollator(feature_extractor)
+
+test_loader = DataLoader(test_ds, batch_size=args.batch_size, collate_fn=collator, 
+                         num_workers=args.num_workers, pin_memory=True)
+
+
+                         
+
+tester(model,test_loader)
+"""avg_acc = total_correct / total_samples
+print("Average accuracy is",avg_acc) """
---
+++ utils.py
@@ -0,0 +1,218 @@
+import argparse
+import torch
+from transformers import ViTConfig, ViTForImageClassification
+from celebA_data_loader import CelebAGenderDataset
+from torch.utils.data import ConcatDataset
+from adience_data_loader import CustomImageDataset,EmptyDataset,ImageClassificationCollator
+from torchvision.transforms import CenterCrop, Resize, Compose
+from torch.utils.data import DataLoader
+import pandas as pd
+from lightning.pytorch.callbacks import ModelCheckpoint,EarlyStopping
+from sklearn.metrics import classification_report, accuracy_score
+import json
+import random
+import lightning as pl
+from transformers import ViTImageProcessor
+from torchvision import transforms
+from pathlib import  Path
+def seed_everything(seed=42):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    pl.seed_everything(seed)
+def tester(model,test_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval()
+    model.to(device)
+    y_test=[]
+    y_pred=[]
+    with torch.no_grad():          
+        for batch_idx, batch in enumerate(test_loader):
+            if(batch_idx%40==0):
+                print(f"DEBUG: Running validation step {batch_idx}", flush=True)
+            pixel_values = batch['pixel_values'].to(device)  
+            labels = batch['labels'].to(device)
+            
+            outputs = model(pixel_values).logits                    
+            preds = outputs.argmax(dim=1)
+            y_pred.extend(preds.cpu().numpy())     
+            y_test.extend(labels.cpu().numpy())
+            """total_correct += (preds == labels).sum().item()
+            total_samples += labels.size(0)"""
+    target_names = ['Female', 'Male']   # for label encoding 0=Female, 1=Male
+
+    report = classification_report(
+        y_test, 
+        y_pred, 
+        target_names=target_names, 
+        digits=2,
+        output_dict=True
+    )
+
+    df = pd.DataFrame(report).T
+    df = df[['precision', 'recall', 'f1-score', 'support']]
+    df.columns = ['Precision', 'Recall', 'F1-Score', 'Support']
+
+
+    df.loc['accuracy', ['Precision', 'Recall', 'F1-Score']] = None
+    df = df.rename(index={'accuracy': 'Accuracy', 
+                        'macro avg': 'Macro avg', 
+                        'weighted avg': 'Weighted avg'})
+
+    print(df.round(4))
+def trainer(model_name):
+    from train_vit import ViT
+    from model_deformConv import CustomDeformViT
+    from model_dilatedConv import CustomDilatedViT
+    from model_addCNN import EnhancedViTForImageClassification
+    from model_VPTshallow import ShallowVPTViTForImageClassification
+    from model_VPTdeep import DeepVPTViTForImageClassification
+    from model_depth import DeepViT
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_dir",     type=str, required=True)
+    parser.add_argument("--attr_file",   type=str, required=True)
+    parser.add_argument("--split_file",  type=str, required=True)
+    #for celebA 
+    parser.add_argument("--checkpoint",  type=str, required=True)
+    #place to store checkpoint
+    parser.add_argument("--dataset",  type=str, required=True)
+    #dataset to use for training
+    parser.add_argument("--start",  type=str, required=True)  
+    #the checkpoint to start from, or start from scratch with input "scratch"
+    parser.add_argument("--import_from",  type=str, default=None)  
+    #the different model to load 
+    parser.add_argument("--epochs",      type=int, default=50)
+    parser.add_argument("--batch_size",  type=int, default=64)
+    parser.add_argument("--dilated_size",type=int, default=2)
+    parser.add_argument("--lr",          type=float, default=1e-4)
+    parser.add_argument("--seed",        type=int, default=42)
+    parser.add_argument("--num_workers", type=int, default=4)
+    args = parser.parse_args()
+    print(json.dumps(vars(args), indent=4))
+
+    model_map = {
+        "ViT":          (ViT,          
+                {"num_classes": 2}),
+        "addCNN":       (EnhancedViTForImageClassification, 
+                {"num_classes": 2,"deformable":True,          
+                "dilation_rates":[1, 2, 4], 
+                "insert_before":True,       
+                "insert_after":False}),
+        "VPTshallow":   (ShallowVPTViTForImageClassification, 
+                {"num_classes": 2, "num_prompts":100,                   
+                "prompt_dropout": 0.1,
+                "freeze_backbone" : True, }),
+        "VPTdeep":      (DeepVPTViTForImageClassification, 
+                { "num_classes": 2,"num_prompts":100,                   
+                "prompt_dropout": 0.2,
+                "freeze_backbone" : True, }),
+        "depth":        (DeepViT,      
+                { "num_classes": 2, "num_extra_layers":8}),
+        "deformConv":   (CustomDeformViT, 
+                { "num_classes": 2,}),
+        "dilatedConv":  (CustomDilatedViT, 
+                { "num_classes": 2,"dilated_size":args.dilated_size}),
+    }
+    seed_everything(args.seed)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    feature_extractor = ViTImageProcessor.from_pretrained('rizvandwiki/gender-classification')
+    train_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandAugment(num_ops=2, magnitude=9),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        
+    ])
+    val_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        
+    ])
+    collator = ImageClassificationCollator(feature_extractor)
+    txt_dir="./label txt"
+    folder = Path(txt_dir)
+    paths = [str(p) for p in folder.iterdir()]
+    if(args.dataset=="adience"):
+        
+        train_ds=EmptyDataset()
+        val_ds=CustomImageDataset(img_dir=args.img_dir,
+                            txt_file=paths[0],transform=val_tf)
+        for p in paths[1:]:
+            train_ds=ConcatDataset([train_ds,CustomImageDataset(img_dir=args.img_dir,
+                            txt_file=p,transform=train_tf)])
+        test_loader  = DataLoader(val_ds,  batch_size=args.batch_size,collate_fn=collator, shuffle=False,
+                              num_workers=args.num_workers, pin_memory=True)
+    elif(args.dataset=="celebA"):
+        train_ds = CelebAGenderDataset(args.img_dir, args.attr_file, args.split_file, split=0, transform=train_tf)
+        val_ds   = CelebAGenderDataset(args.img_dir, args.attr_file, args.split_file, split=1, transform=val_tf)
+        test_ds   = CelebAGenderDataset(args.img_dir, args.attr_file, args.split_file, split=2, transform=val_tf)
+        test_loader  = DataLoader(test_ds,  batch_size=args.batch_size,collate_fn=collator, shuffle=False,
+                              num_workers=args.num_workers, pin_memory=True)
+    elif(args.dataset=="both"):
+        train_ds=EmptyDataset()
+        for p in paths[1:]:
+            train_ds=ConcatDataset([train_ds,CustomImageDataset(img_dir="./aligned",
+                            txt_file=p,transform=train_tf)])
+        train_ds=ConcatDataset([train_ds,CelebAGenderDataset(args.img_dir, args.attr_file, args.split_file, split=0, transform=train_tf)])
+        val_ds=CustomImageDataset(img_dir="./aligned",
+                            txt_file=paths[0],transform=val_tf)
+        test_loader  = DataLoader(val_ds,  batch_size=args.batch_size,collate_fn=collator, shuffle=False,
+                              num_workers=args.num_workers, pin_memory=True) 
+
+    train_loader = DataLoader(train_ds, batch_size=args.batch_size,  collate_fn=collator,shuffle=True,
+                              num_workers=args.num_workers, pin_memory=True)
+    val_loader   = DataLoader(val_ds,   batch_size=args.batch_size,collate_fn=collator, shuffle=False,
+                              num_workers=args.num_workers, pin_memory=True)
+    
+    config = ViTConfig(
+        image_size=224,
+        patch_size=16,
+        num_channels=3,
+        
+        hidden_size=384,        
+        num_hidden_layers=12,    
+        num_attention_heads=12,  
+        intermediate_size=3072, 
+        
+        num_labels=2,
+
+        hidden_dropout_prob=0.1,         
+        attention_probs_dropout_prob=0.1,  
+        drop_path_rate=0.1,
+    )
+    cls, extra_kwargs = model_map[model_name]
+    if(args.start=="scratch"):
+        model=cls(config=config,epochs=args.epochs,**extra_kwargs)
+    elif(args.start=="local" or args.start=="online"):
+        model = cls(
+             config=args.start,model_name=args.import_from,epochs=args.epochs,
+            **extra_kwargs     
+        )
+    else:
+        model=cls.load_from_checkpoint(args.start)
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_acc',     
+        mode='max',                 
+        dirpath=args.checkpoint,
+        filename='vit-best-{epoch:02d}-{val_accuracy:.4f}',
+        save_top_k=1,               # save best model
+        save_last=True,             
+        auto_insert_metric_name=False,
+        verbose=True
+    )
+    early_stop_callback = EarlyStopping(
+        monitor='val_acc',      
+        min_delta=0.00,         
+        patience=3,             
+        verbose=True,           
+        mode='max'              
+    )
+    model = model.to(device)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, precision=16, max_epochs=args.epochs,
+    enable_progress_bar=False,log_every_n_steps=1,val_check_interval=1.0,
+    check_val_every_n_epoch=3,callbacks=[checkpoint_callback,early_stop_callback],
+    accumulate_grad_batches=4)
+    print("Starting Trainer.fit()...", flush=True)
+    
+    trainer.fit(model, train_loader, val_loader)
+    tester(model,test_loader)
